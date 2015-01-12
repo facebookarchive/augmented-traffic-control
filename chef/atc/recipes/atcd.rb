c3ad828d678c4bd40906bb25562ecd868e6e3e69
Zeal Jagannatha <zealjagannatha@gmail.com>
2015-01-12 14:09:26 -0800

    Atcd cookbook dynamically finds interfaces

--- chef/atc/recipes/atcd.rb
+++ chef/atc/recipes/atcd.rb
@@ -17,12 +17,11 @@ include_recipe 'atc::_virtualenv'
 # Set sysctl values.
 node.default['sysctl']['params']['net']['ipv4']['ip_forward'] = 1
 node.default['sysctl']['allow_sysctl_conf'] = true
-node['network']['interfaces'].keys.sort.each do |eth|
-  next unless eth.start_with?('eth')
-  node.default['sysctl']['params']['net']['ipv4']['conf'][eth]\
-    ['arp_ignore'] = 1
-  node.default['sysctl']['params']['net']['ipv4']['conf'][eth]\
-    ['arp_announce'] = 2
+node.network.interfaces.each do |name, eth|
+    if eth.encapsulation == "Ethernet"
+        node.default['sysctl']['params']['net']['ipv4']['conf'][name]['arp_ignore'] = 1
+        node.default['sysctl']['params']['net']['ipv4']['conf'][name]['arp_announce'] = 2
+    end
 end
 
 # If we're in a sandbox, i.e. vagrant, we want to set up NAT
@@ -30,47 +29,62 @@ end
 # We need NAT because we have just a bunch of VMs (atcclients) without a router
 # so the atcd box need to handle the routing/NAT
 if node.vagrant?
-  # NAT
-  include_recipe 'simple_iptables'
-  simple_iptables_rule 'nat' do
-    table 'nat'
-    direction 'POSTROUTING'
-    jump 'MASQUERADE'
-    rule "-o #{node['network']['default_interface']}"
-  end
-  # DHCP
-  # By providing IP on eth1, we can get Genymotion instances to route
-  # through atc
-  # default to eth1
-  eth = 'eth1'
-  require 'ipaddr'
-  node.default['dhcp']['interfaces'] = [eth]
-  include_recipe 'dhcp::server'
 
-  ipv4 = node['network']['interfaces'][eth]['addresses'].select do
-    |_, v| v.family == 'inet'
-  end
-  ip = ipv4.keys[0]
-  ipaddr = IPAddr.new "#{ip}/#{ipv4[ip]['prefixlen']}"
-  range_start = ipaddr | 100
-  range_end = ipaddr | 200
-  dhcp_subnet ipaddr.to_s do
-    range "#{range_start} #{range_end}"
-    broadcast ipv4[ip]['broadcast']
-    netmask ipv4[ip]['netmask']
-    routers [ip]
-  end
+    # Try to guess the correct interfaces.
+    lan_iface = ''
+    wan_iface = node.network.default_interface
+    node.network.interfaces.each do |name,eth|
+        lan_iface = name if (eth.encapsulation == "Ethernet") and (name != wan_iface) 
+    end
 
-  # When running under vagrant, atcd depends on the mounts and will not start
-  # unless those are up. The mount is happening after the system is up.
-  # We can use udev to trigger starting/stopping atcd when amount/umount event
-  # happens.
-  template '/etc/udev/rules.d/50-vagrant-mount-atcd.rules' do
-    source 'mount-udev.rules.erb'
-    variables(
-      :service => 'atcd'
-    )
-  end
+    if lan_iface == ''
+        log "You only have 1 network interface. ATCD requires 2 network interfaces. Chef will likely fail."
+        lan_iface = 'eth1'
+    end
+
+    puts "Using interfaces lan=>#{lan_iface}, wan=>#{wan_iface}"
+    log "Using interfaces lan=>#{lan_iface}, wan=>#{wan_iface}"
+
+    # NAT
+    include_recipe 'simple_iptables'
+    simple_iptables_rule 'nat' do
+        table 'nat'
+        direction 'POSTROUTING'
+        jump 'MASQUERADE'
+        rule "-o #{node['network']['default_interface']}"
+    end
+
+    # DHCP
+    # By providing IP on the lan interface, we can get Genymotion instances to route
+    # through atc
+    require 'ipaddr'
+    node.default['dhcp']['interfaces'] = [lan_iface]
+    include_recipe 'dhcp::server'
+
+    ipv4 = node['network']['interfaces'][lan_iface]['addresses'].select do |k, v|
+        v.family == 'inet'
+    end
+    ip = ipv4.keys[0]
+    ipaddr = IPAddr.new "#{ip}/#{ipv4[ip]['prefixlen']}"
+    range_start = ipaddr | 100
+    range_end = ipaddr | 200
+    dhcp_subnet ipaddr.to_s do
+        range "#{range_start} #{range_end}"
+        broadcast ipv4[ip]['broadcast']
+        netmask ipv4[ip]['netmask']
+        routers [ip]
+    end
+
+    # When running under vagrant, atcd depends on the mounts and will not start
+    # unless those are up. The mount is happening after the system is up.
+    # We can use udev to trigger starting/stopping atcd when amount/umount event
+    # happens.
+    template '/etc/udev/rules.d/50-vagrant-mount-atcd.rules' do
+        source 'mount-udev.rules.erb'
+        variables({
+          :service => 'atcd'
+        })
+    end
 end
 
 include_recipe 'sysctl::apply'
@@ -83,29 +97,29 @@ end
 
 # Ensure the Sqlite directory exists
 directory File.dirname(node['atc']['atcd']['sqlite']) do
-  owner 'root'
-  group 'root'
-  mode 00755
-  recursive true
+    owner 'root'
+    group 'root'
+    mode 00755
+    recursive true
 end
 
 template node['atc']['atcd']['config_file'] do
-  source 'config/atcd.erb'
-  mode 0755
-  owner 'root'
-  group 'root'
-  notifies :restart, 'service[atcd]', :delayed
+    source 'config/atcd.erb'
+    mode 0755
+    owner 'root'
+    group 'root'
+    notifies :restart, 'service[atcd]', :delayed
 end
 
-cookbook_file '/etc/init.d/atcd' do
-  source "init.d/atcd.#{node['platform_family']}"
-  mode 0755
-  owner 'root'
-  group 'root'
-  notifies :restart, 'service[atcd]', :delayed
+cookbook_file "/etc/init.d/atcd" do
+    source "init.d/atcd.#{node['platform_family']}"
+    mode 0755
+    owner "root"
+    group "root"
+    notifies :restart, 'service[atcd]', :delayed
 end
 
-service 'atcd' do
-  supports :restart => true
-  action [:enable, :start]
+service "atcd" do
+    supports :restart => true
+    action [:enable, :start]
 end
