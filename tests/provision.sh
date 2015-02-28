a91a90803aac93a25607909bda7342f4622ef4ac
Zeal Jagannatha <zealjagannatha@gmail.com>
2015-02-28 07:25:36 -0800

    Licenced test files.

--- tests/provision.sh
+++ tests/provision.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -eu
 
+#
+#  Copyright (c) 2014, Facebook, Inc.
+#  All rights reserved.
+#
+#  This source code is licensed under the BSD-style license found in the
+#  LICENSE file in the root directory of this source tree. An additional grant
+#  of patent rights can be found in the PATENTS file in the same directory.
+#
+#
+
 if [[ "$(hostname)" == "gateway" ]] ; then
     sed -i -e 's/ATCD_WAN=eth[012]/ATCD_WAN=eth1/' -e 's/ATCD_LAN=eth[012]/ATCD_LAN=eth2/' /etc/default/atcd
 
