5763ebf4a65428ee96c8f3c471c42680e40c4953
chantra <chantra@fb.com>
2015-05-13 13:45:28 -0700

    Create a script to dump system config
    Also change the VM mountpoint so we can access the utils directory from
    the vm

---
+++ utils/dump_system_config.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+WAN=${1:-eth0}
+LAN=${2:-eth1}
+
+function title {
+    echo
+    echo "######### $1 #########"
+    echo
+}
+
+function run_cmd {
+    title "$1"
+    $1
+}
+
+for cmd in "ip a" "ip r" "iptables-save"
+do
+    run_cmd "${cmd}"
+done
+
+for netif in ${WAN} ${LAN}
+do
+    for object in qdisc class filter
+    do
+        cmd="tc ${object} show dev ${netif}"
+        run_cmd "${cmd}"
+    done
+done
