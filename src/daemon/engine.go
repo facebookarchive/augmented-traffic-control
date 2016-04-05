53225f205a81fc9046379f9d6576f13173d1a90b
Zeal Jagannatha <zealjagannatha@gmail.com>
2016-04-05 13:40:19 -0700

    Added periodic hook type and resolution flag

--- src/daemon/engine.go
+++ src/daemon/engine.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"time"
 
 	"github.com/facebook/augmented-traffic-control/src/atc_thrift"
 	"github.com/facebook/augmented-traffic-control/src/shaping"
@@ -14,9 +15,10 @@ type ShapingEngine struct {
 	conf       *Config
 	shaper     shaping.Shaper
 	hooks      [][]Hook
+	resolution int
 }
 
-func NewShapingEngine(thriftAddr *net.TCPAddr, conf *Config) (*ShapingEngine, error) {
+func NewShapingEngine(resolution int, thriftAddr *net.TCPAddr, conf *Config) (*ShapingEngine, error) {
 	shaper, err := shaping.GetShaper()
 	if err != nil {
 		return nil, err
@@ -24,11 +26,15 @@ func NewShapingEngine(thriftAddr *net.TCPAddr, conf *Config) (*ShapingEngine, er
 	if err := shaper.Initialize(); err != nil {
 		return nil, err
 	}
-	return buildShapingEngine(thriftAddr, shaper, conf)
+	return buildShapingEngine(resolution, thriftAddr, shaper, conf)
 }
 
 // Separate than NewShapingEngine so that tests have an entrypoint.
-func buildShapingEngine(thriftAddr *net.TCPAddr, shaper shaping.Shaper, conf *Config) (*ShapingEngine, error) {
+func buildShapingEngine(resolution int, thriftAddr *net.TCPAddr, shaper shaping.Shaper, conf *Config) (*ShapingEngine, error) {
+	if resolution <= 0 {
+		// resolution of 0 is bad!
+		resolution = 1
+	}
 	hooks := make([][]Hook, len(all_hook_types))
 	for i := range all_hook_types {
 		hooks[i] = make([]Hook, 0, 5)
@@ -42,12 +48,15 @@ func buildShapingEngine(thriftAddr *net.TCPAddr, shaper shaping.Shaper, conf *Co
 			hooks[t] = append(hooks[t], hook)
 		}
 	}
-	return &ShapingEngine{
+	eng := &ShapingEngine{
 		thriftAddr: thriftAddr,
 		conf:       conf,
 		shaper:     shaper,
 		hooks:      hooks,
-	}, nil
+		resolution: resolution,
+	}
+	go eng.runPeriodicHooks(time.Unix(0, 0))
+	return eng, nil
 }
 
 func (eng *ShapingEngine) GetPlatform() atc_thrift.PlatformType {
@@ -92,8 +101,14 @@ func (eng *ShapingEngine) runHooks(t HookType, id int64, addr shaping.Target) er
 	if len(eng.hooks) >= int(t) {
 		env := eng.buildHookEnv(t, addr)
 		for _, hook := range eng.hooks[t] {
-			if err := hook.Run(env, fmt.Sprintf("%d", id), addr.String()); err != nil {
-				return err
+			if id != -1 {
+				if err := hook.Run(env, fmt.Sprintf("%d", id), addr.String()); err != nil {
+					return err
+				}
+			} else {
+				if err := hook.Run(env); err != nil {
+					return err
+				}
 			}
 		}
 	}
@@ -106,5 +121,21 @@ func (eng *ShapingEngine) buildHookEnv(t HookType, addr shaping.Target) []string
 		fmt.Sprintf("ATCD_PATH=%s", os.Args[0]),
 		fmt.Sprintf("ATC_MEMBER=%s", addr),
 		fmt.Sprintf("ATC_HOOK_TYPE=%s", t.String()),
+	}
+}
+
+func (eng *ShapingEngine) runPeriodicHooks(startTime time.Time) {
+	if startTime.After(time.Now()) {
+		Log.Debugf("Waiting until %s to begin running periodic hooks", startTime.Format(time.ANSIC))
+		for time.Now().Before(startTime) {
+			time.Sleep(time.Duration(eng.resolution) * time.Second)
+		}
+	}
+	Log.Debugf("Running periodic hooks")
+	for {
+		// In case of a hook failure, we log that it failed and the stdout/err
+		// so we safely discard the error here
+		eng.runHooks(PERIODIC, -1, nil)
+		time.Sleep(time.Duration(eng.resolution) * time.Second)
 	}
 }
