919dd8d27c09bfa6e6719dd1eb5d060816b9e3f9
chantra <chantr4@gmail.com>
2017-06-16 14:33:09 -0700

    remove unused docker build

--- docker/start.sh
+++
@@ -1,8 +0,0 @@
-sed -i -e "s/{{ATCD_WAN}}/${ATCD_WAN}/
-    s/{{ATCD_LAN}}/${ATCD_LAN}/
-    s/{{ATCD_MODE}}/${ATCD_MODE}/
-    s/{{ATCD_OPTIONS}}/${ATCD_OPTIONS}/
-    s/{{ATCD_BURST_SIZE}}/${ATCD_BURST_SIZE}/" ./supervisord.conf
-
-# start all the services
-/usr/local/bin/supervisord -n
